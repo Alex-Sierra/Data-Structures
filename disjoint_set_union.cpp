0f2d7543666d9fed470b3e042482cadf93065bd1
asciber45 <alex.sierra@estudiantes.matcom.uh.cu>
2022-11-18 00:49:05 -0500

    dsu

--- disjoint_set_union.cpp
+++ disjoint_set_union.cpp
@@ -1,10 +1,6 @@
 #include <bits/stdc++.h>
  
-#define MAX(a, b) (a > b) ? a : b
-#define MIN(a, b) (a < b) ? a : b
- 
 using namespace std;
-typedef long long ll;
 typedef vector<int> vi;
  
 struct dsu {
@@ -25,39 +21,6 @@ struct dsu {
                 swap(a, b);
             p[a] += p[b];
             p[b] = a;
-        }
-    }
-};
-
-struct DSU {
-    vi p, rank;
-    DSU(int n) {
-        p.resize(n);
-        rank.resize(n);
-        for (int i = 0; i < n; i++) {
-            p[i] = i;
-            rank[i] = 0;
-        }
-    }
-    int find(int x) {
-        if (p[x] != x) {
-            p[x] = find(p[x]);
-        }
-        return p[x];
-    }
-    void unite(int x, int y) {
-        int xroot = find(x);
-        int yroot = find(y);
-        if (xroot == yroot) {
-            return;
-        }
-        if (rank[xroot] < rank[yroot]) {
-            p[xroot] = yroot;
-        } else if (rank[xroot] > rank[yroot]) {
-            p[yroot] = xroot;
-        } else {
-            p[yroot] = xroot;
-            rank[xroot]++;
         }
     }
 };
