ff72b308b5556b8453a07ecc7da1c19ed8cf092d
asciber45 <alex.sierra@estudiantes.matcom.uh.cu>
2022-12-07 17:56:25 -0500

    find the kth

--- trie.cpp
+++ trie.cpp
@@ -5,25 +5,29 @@ using namespace std;
 struct trie {
     struct node {
         int ocurrences;
+        int count;
         map<char, node*> next;
         node() {
             ocurrences = 0;
+            count = 0;
         }
     };
     node *root;
     trie() {
         root = new node();
     }
-    void add(string &s) {
+    void add(string s) {
         node *cur = root;
         for (char c : s) {
+            cur->count++;
             if (cur->next.find(c) == cur->next.end())
                 cur->next[c] = new node();
             cur = cur->next[c];
         }
         cur->ocurrences++;
+        cur->count++;
     }
-    bool delete_(string &s) {
+    bool delete_(string s) {
         node *cur = root;
         for (char c : s) {
             if (cur->next.find(c) == cur->next.end())
@@ -31,12 +35,18 @@ struct trie {
             cur = cur->next[c];
         }
         if (cur->ocurrences != 0) {
+            cur = root;
+            for (char c : s) {
+                cur->count--;
+                cur = cur->next[c];
+            }
             cur->ocurrences--;
+            cur->count--;
             return true;
         }
         return false;
     }
-    int find(string &s) {
+    int find(string s) {
         node *cur = root;
         for (char c : s) {
             if (cur->next.find(c) == cur->next.end())
@@ -45,7 +55,7 @@ struct trie {
         }
         return cur->ocurrences;
     }
-    string find_prefix(string &s) {
+    string find_prefix(string s) {
         node *cur = root;
         string result;
         for (char c : s) {
@@ -53,6 +63,51 @@ struct trie {
                 break;
             cur = cur->next[c];
             result += c;
+        }
+        return result;
+    }
+    vector<string> find_suffixes(string s) {
+        node *cur = root;
+        for (char c : s) {
+            if (cur->next.find(c) == cur->next.end())
+                return {};
+            cur = cur->next[c];
+        }
+        vector<string> result;
+        function<void(node*, string)> dfs = [&](node *cur, string s) {
+            if (cur->ocurrences != 0)
+                result.push_back(s);
+            for (auto p : cur->next)
+                dfs(p.second, s + p.first);
+        };
+        dfs(cur, "");
+        return result;
+    }
+    int count_prefix(string s) {
+        node *cur = root;
+        for (char c : s) {
+            if (cur->next.find(c) == cur->next.end())
+                return 0;
+            cur = cur->next[c];
+        }
+        return cur->count;
+    }
+    string find_kth(int k) {
+        node *cur = root;
+        string result;
+        while (k > 0) {
+            vector<pair<char, node*>> v(cur->next.begin(), cur->next.end());
+            sort(v.begin(), v.end());
+            if (v.empty())
+                break;
+            for (auto p : v) {
+                if (p.second->count >= k) {
+                    result += p.first;
+                    cur = p.second;
+                    break;
+                }
+                k -= p.second->count;
+            }
         }
         return result;
     }
