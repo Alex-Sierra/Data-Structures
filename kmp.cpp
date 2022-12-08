23bbc767fc3137394265c6480c165ed329b34d43
asciber45 <alex.sierra@estudiantes.matcom.uh.cu>
2022-12-08 10:08:16 -0500

    knuth-morris-pratt

---
+++ kmp.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+vector<int> kmp(string text, string pattern) {
+    int n = text.size();
+    int m = pattern.size();
+    vector<int> pi(m);
+    vector<int> result;
+    for (int i = 1; i < m; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && pattern[i] != pattern[j])
+            j = pi[j - 1];
+        if (pattern[i] == pattern[j])
+            j++;
+        pi[i] = j;
+    }
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        while (j > 0 && text[i] != pattern[j])
+            j = pi[j - 1];
+        if (text[i] == pattern[j])
+            j++;
+        if (j == m) {
+            result.push_back(i - m + 1);
+            j = pi[j - 1];
+        }
+    }
+    return result;
+}
