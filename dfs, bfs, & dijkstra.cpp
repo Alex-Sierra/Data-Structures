3db687e4293b30b8cc4d479dd7e545c897593488
Alex <alexsierraalcala@gmail.com>
2023-02-15 02:49:30 -0500

    dfs, bfs, dijkstra

---
+++ dfs, bfs, & dijkstra.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#define MAX(a, b) (a > b) ? a : b
+#define MIN(a, b) (a < b) ? a : b
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define per(i, a, b) for (int i = (a); i >= (b); --i)
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+// dfs with adjacency list and stack
+void dfs(vector<vi> adj, int s) {
+    vector<bool> visited(adj.size(), false);
+    stack<int> st;
+    st.push(s);
+    visited[s] = true;
+    while (!st.empty()) {
+        int u = st.top();
+        st.pop();
+        if (!visited[u]) {
+            visited[u] = true;
+            for (int v : adj[u]) {
+                if (!visited[v]) {
+                    st.push(v);
+                }
+            }
+        }
+    }
+}
+
+// bfs with adjacency list and queue
+void bfs(vector<vi> adj, int s) {
+    vector<bool> visited(adj.size(), false);
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        if (!visited[u]) {
+            visited[u] = true;
+            for (int v : adj[u]) {
+                if (!visited[v]) {
+                    q.push(v);
+                }
+            }
+        }
+    }
+}
+
+// dijkstra with adjacency list and priority queue
+void dijkstra(vector<vii> adj, int s) {
+    vi dist(adj.size(), INT_MAX);
+    priority_queue<ii, vii, greater<ii>> pq;
+    pq.push({s, 0});
+    dist[s] = 0;
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+        for (ii v : adj[u]) {
+            if (dist[u] + v.second < dist[v.first]) {
+                dist[v.first] = dist[u] + v.second;
+                pq.push({dist[v.first], v.first});
+            }
+        }
+    }
+}
